815c467966d5f7487a7f9babd53168a2023b0a17
Enoc Mena <enocschool@Enocs-iMac.local>
2021-06-10 16:25:06 -0400

    Initial commit

---
+++ src/test/java/com/ex32/GuessingGameTest.java
@@ -0,0 +1,22 @@
+package com.ex32;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessingGameTest {
+
+    @org.junit.jupiter.api.Test
+    void choiceOne() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void choiceTwo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void choiceThree() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void playAgain() {
+    }
+}
